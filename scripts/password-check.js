2ad08cb3076c4d10c29006ffe97b819c407221c1
Julian Kruithof <julian.kruithof@hva.nl>
2022-12-17 21:42:47 +0100

    improved password-check

--- scripts/password-check.js
+++ scripts/password-check.js
@@ -1,86 +1,60 @@
 const input = document.getElementById("password");
+//create an array with all checks
+const requirements = [
+	{
+		type: "length",
+		check: (value) => value.length >= 8,
+	},
+	{
+		type: "lower",
+		check: (value) => value.match(/[a-z]+/) !== null,
+	},
+	{
+		type: "upper",
+		check: (value) => value.match(/[A-Z]+/) !== null,
+	},
+	{
+		type: "number",
+		check: (value) => value.match(/[0-9]+/) !== null,
+	},
+	{
+		type: "symbol",
+		check: (value) => value.match(/[!@#$%^&*?.,+_-]+/) !== null,
+	},
+];
+
+//check password on input of the a password
 input.addEventListener("input", () => {
 	const value = input.value;
-	const Object = checkRequirements(value);
-	if (Object.lengthCheck) {
-		const listItem = document.querySelector('[data-check="length"]');
-		styleCheckTrue(listItem);
-	} else {
-		const listItem = document.querySelector('[data-check="length"]');
-		resetItem(listItem);
-	}
-	if (Object.lowerCheck) {
-		const listItem = document.querySelector('[data-check="lower"]');
-		styleCheckTrue(listItem);
-	} else {
-		const listItem = document.querySelector('[data-check="lower"]');
-		resetItem(listItem);
-	}
-	if (Object.upperCheck) {
-		const listItem = document.querySelector('[data-check="upper"]');
-		styleCheckTrue(listItem);
-	} else {
-		const listItem = document.querySelector('[data-check="upper"]');
-		resetItem(listItem);
-	}
-	if (Object.numberCheck) {
-		const listItem = document.querySelector('[data-check="number"]');
-		styleCheckTrue(listItem);
-	} else {
-		const listItem = document.querySelector('[data-check="number"]');
-		resetItem(listItem);
-	}
-	if (Object.symbolCheck) {
-		const listItem = document.querySelector('[data-check="symbol"]');
-		styleCheckTrue(listItem);
-	} else {
-		const listItem = document.querySelector('[data-check="symbol"]');
-		resetItem(listItem);
-	}
+	requirements.forEach(({ type, check }) => {
+		const listItem = document.querySelector(`[data-check="${type}"]`);
+		if (check(value)) {
+			updateListItem(listItem, "green");
+		} else {
+			updateListItem(listItem, "red");
+		}
+	});
 });
 
-function checkRequirements(value) {
-	let passwordChecks = {
-		lengthCheck: false,
-		lowerCheck: false,
-		upperCheck: false,
-		numberCheck: false,
-		symbolCheck: false,
-	};
-	if (value.length >= 8) {
-		passwordChecks.lengthCheck = true;
-	}
-	if (value.match(/[a-z]+/) !== null) {
-		passwordChecks.lowerCheck = true;
-	}
-	if (value.match(/[A-Z]+/) !== null) {
-		passwordChecks.upperCheck = true;
-	}
-	if (value.match(/[0-9]+/) !== null) {
-		passwordChecks.numberCheck = true;
-	}
-	if (value.match(/[!@#$%^&*?.,+_-]+/) !== null) {
-		passwordChecks.symbolCheck = true;
-	}
-	return passwordChecks;
-}
-function getColorValue(prop) {
-	const colors = {
-		greenColor: prop.getPropertyValue("--color-green"),
-		redColor: prop.getPropertyValue("--color-red"),
-	};
-	return colors;
-}
-function resetItem(item) {
-	const colors = getColorValue(getComputedStyle(document.body));
-	item.style.setProperty("color", colors.redColor);
-	item.firstElementChild.classList.remove("fa-check");
-	item.firstElementChild.classList.add("fa-circle-xmark");
+/**
+ * update the item on the list when depending on wether the check is matched or not
+ * @param {HTMLLIElement} item
+ * @param {string} color name of a color
+ */
+function updateListItem(item, color) {
+	const { greenColor, redColor } = getColorValues();
+	item.style.setProperty("color", color === "green" ? greenColor : redColor);
+	item.firstElementChild.classList.toggle("fa-check", color === "green");
+	item.firstElementChild.classList.toggle("fa-circle-xmark", color === "red");
 }
 
-function styleCheckTrue(item) {
-	const colors = getColorValue(getComputedStyle(document.body));
-	item.style.setProperty("color", colors.greenColor);
-	item.firstElementChild.classList.add("fa-check");
-	item.firstElementChild.classList.remove("fa-circle-xmark");
+/**
+ * create an object containing the values of the a green and a red color
+ * @returns object with color values.
+ */
+function getColorValues() {
+	return {
+		greenColor: getComputedStyle(document.body).getPropertyValue("--color-green"),
+		redColor: getComputedStyle(document.body).getPropertyValue("--color-red"),
+	};
 }
