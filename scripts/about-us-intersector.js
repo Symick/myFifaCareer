dd4cd2c50ff36792033c47e07bdcc0adcc4ceb7a
Julian Kruithof <julian.kruithof@hva.nl>
2022-12-17 21:21:36 +0100

    improved intersectionobserver

--- scripts/about-us-intersector.js
+++ scripts/about-us-intersector.js
@@ -1,32 +1,28 @@
 /*
   code for about Us observer, observer checks if the window is intersecting the about-us page,
-  if about us is 95 percent of the screen, the nav-style will change. 
+  if about us is 95 percent of the screen, the nav-style will change.
 */
 const aboutUs = document.querySelector(".about-us");
+const header = document.querySelector(".header");
+//precentage of the screen taken by the header
+const sizeTakenByHeader = header.clientHeight / window.innerHeight;
 
 const aboutUsObserver = new IntersectionObserver(
 	function (entries, aboutUsObserver) {
-		const header = document.querySelector(".header");
 		let scrollbarColor = "--scrollbar-color";
 
 		entries.forEach((entry) => {
 			if (!entry.isIntersecting) {
 				header.classList.remove("nav-at--about-us");
-				document.documentElement.style.setProperty(
-					scrollbarColor,
-					"#030564"
-				);
+				document.documentElement.style.setProperty(scrollbarColor, "#030564");
 			} else {
 				header.classList.add("nav-at--about-us");
-				document.documentElement.style.setProperty(
-					scrollbarColor,
-					"#640305"
-				);
+				document.documentElement.style.setProperty(scrollbarColor, "#640305");
 			}
 		});
 	},
 	{
-		threshold: 0.95,
+		threshold: 1 - sizeTakenByHeader,
 	}
 );
 
